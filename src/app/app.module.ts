eeaa751eb8d8127c755e73266e8b0fe8b49679e0
allamch <51353082+allamch@users.noreply.github.com>
2020-02-26 12:30:11 +0100

    template integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonalComponent } from './testimonal/testimonal.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServiceComponent,
+    PortfolioComponent,
+    TestimonalComponent,
+    TeamComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
